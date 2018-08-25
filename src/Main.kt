2e5ae07f9235ea1d015839f77cb8912230474846
septjose <septjose@hotmail.com>
2018-08-24 23:03:04 -0500

    almost almost

--- src/Main.kt
+++ src/Main.kt
@@ -5,7 +5,6 @@ import Process.Orthogonality
 
 fun main(args: Array<String>) {
     //definicion de patrones
-
     val m1 = arrayOf(intArrayOf(1, 1,-1,1), intArrayOf(-1, 1,1,1), intArrayOf(-1, 1,1,1),intArrayOf(1, 1,1,-1))
     val m2 = arrayOf(intArrayOf(1, 1,-1,1), intArrayOf(-1, 1,1,1), intArrayOf(-1, 1,1,1),intArrayOf(1, 1,-1,1))
 
@@ -13,10 +12,9 @@ fun main(args: Array<String>) {
     val m= Multiplication()
 
     val mul = Weights()
-   val orth = Orthogonality()
+    val orth = Orthogonality()
     val n= m1.size*m1[0].size
     println(n*0.138)
-
 
 
     //recuperacion
@@ -32,30 +30,10 @@ fun main(args: Array<String>) {
 
             if(orth.checkOrthogonality(m1,m2)<=0)
             {
-                // definicion de pesos
-               val weights= mul.Weights(m1,m2)
-                //funcionamiento
-
-                val fe =m.multiplyMatrices(pf,weights)
-
-                val resultado = Array(fe.size) { IntArray(fe[0].size) }
-                for (x in 0 until resultado.size) {
-                    for (y in 0 until resultado[x].size) {
-
-                   if (fe[x][y]>0)
-                   {
-                       resultado[x][y] = 1
-
-                   }else
-                   {
-                       resultado[x][y] = -1
-                   }
-
-                    }
-                }
+               println("Es ortogonal")
             }
             else
-                println("No satistace la ortogonalidad")
+                println("No es ortogonal")
 
         }else
         {
@@ -65,26 +43,19 @@ fun main(args: Array<String>) {
     }else
         println("recuperacion mala ")
 
-
     val weights= mul.Weights(m1,m2)
     //funcionamiento
-
     val fe =m.multiplyMatrices(pf,weights)
-
     val resultado = Array(fe.size) { IntArray(fe[0].size) }
     for (x in 0 until resultado.size) {
         for (y in 0 until resultado[x].size) {
-
             if (fe[x][y]>0)
             {
                 resultado[x][y] = 1
-
             }else
             {
                 resultado[x][y] = -1
             }
-
         }
     }
-
 }
